bc9e8d19cdfc7bf226c10bbc4edad7ce89573902
seblex9 <seblex@proton.me>
2023-10-11 21:52:56 +0800

    Get joke from API using fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+export default function App() {
+  async function getJoke() {
+    const res = await fetch('https://api.chucknorris.io/jokes/random');
+    const data = await res.json();
+    console.log(data.value);
+  }
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Chuckles With Chuck</h1>
+      <button onClick={getJoke}>Get Joke</button>
     </div>
   );
 }
-
-export default App;
