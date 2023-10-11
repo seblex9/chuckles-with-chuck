5e18fb44a749d49d9388d15842d3243d236e7373
seblex9 <seblex@proton.me>
2023-10-11 22:29:04 +0800

    Add style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { useState } from 'react';
+
 export default function App() {
+  const [joke, setJoke] = useState('');
+
   async function getJoke() {
     const res = await fetch('https://api.chucknorris.io/jokes/random');
     const data = await res.json();
-    console.log(data.value);
+    setJoke(data.value);
   }
 
   return (
     <div>
-      <h1>Chuckles With Chuck</h1>
-      <button onClick={getJoke}>Get Joke</button>
+      <h1 className='app-title'>Chuckles With Chuck</h1>{' '}
+      <div className='app-container'>
+        <h3 className='joke-text'>{joke}</h3>
+        <button className='get-joke-button' onClick={getJoke}>
+          Get Joke
+        </button>
+      </div>
     </div>
   );
 }
